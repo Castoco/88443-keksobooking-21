cd1c7a85033c2c0113f998378344e2679a1d8671
Evgeniy <casto2010@yandex.ru>
2020-10-13 14:42:09 +0300

    Внес правки по замечаниям

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ const TYPE_HOTEL = ['palace', 'flat', 'house', 'bungalow'];
 const TITLE_WORDS = ['Номер', 'Хата', 'Дыра', 'Квартира'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const HOTEL_PHOTO = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-const MAP_WIDTH = 900;
+const MAP_WIDTH = 1200;
+const PIN_FIELD = MAP_WIDTH - PIN_WIDTH;
 const MAP_START_X = 0;
 const MAP_TOP_Y = 130;
-const MAP_BOTTOM_Y = 620;
+const MAP_BOTTOM_Y = 630;
 const TIME_CHECKIN = 12;
 const TIME_CHECKOUT = 14;
 const MIN_PRICE = 100;
@@ -29,7 +30,7 @@ const getRandomNumber = function (min, max) {
 
 document.querySelector(`.map`).classList.remove(`map--faded`);
 
-const getStrLength = function (arr) {
+const getStrRandom = function (arr) {
   let randomStr;
   randomStr = arr.slice(0, getRandomNumber(0, arr.length));
 
@@ -37,7 +38,7 @@ const getStrLength = function (arr) {
 };
 
 const randomPin = function (counter) {
-  const pinLocationX = getRandomNumber(MAP_START_X, MAP_WIDTH);
+  const pinLocationX = getRandomNumber(MAP_START_X, PIN_FIELD);
   const pinLocationY = getRandomNumber(MAP_TOP_Y, MAP_BOTTOM_Y);
   const announcement =
     {
@@ -48,14 +49,14 @@ const randomPin = function (counter) {
         'title': `Это лучшая(-ый) ${TITLE_WORDS[getRandomNumber(0, TITLE_WORDS.length)]} во всем Токио!`,
         'address': `Адрес предложения ${pinLocationX}, ${pinLocationY}`,
         'price': getRandomNumber(MIN_PRICE, MAX_PRICE),
-        'type': TYPE_HOTEL[getRandomNumber(0, TYPE_HOTEL.length)],
+        'type': TYPE_HOTEL[getRandomNumber(1, TYPE_HOTEL.length)],
         'rooms': getRandomNumber(ROOMS_MIN, ROOMS_MAX),
         'guests': getRandomNumber(GUESTS_MIN, GUESTS_MAX),
         'checkin': `${getRandomNumber(TIME_CHECKIN, TIME_CHECKOUT)}:00`,
         'checkout': `${getRandomNumber(TIME_CHECKIN, TIME_CHECKOUT)}:00`,
-        'features': getStrLength(FEATURES),
+        'features': getStrRandom(FEATURES),
         'description': `В квартире есть все для комфортного проживания, СКИДКИ ОТ 3 ДНЕЙ И ПОСТОЯННЫМ КЛИЕНТАМ!!!`,
-        'photos': getStrLength(HOTEL_PHOTO)
+        'photos': getStrRandom(HOTEL_PHOTO)
       },
       'location': {
         'x': pinLocationX,
@@ -80,8 +81,8 @@ const pinTemplate = document.querySelector(`#pin`).content.querySelector(`.map__
 const mapList = document.querySelector(`.map__pins`);
 
 const renderElement = function (render) {
-  let element = pinTemplate.cloneNode(true);
-  element.style = `left: ${render.location.x + PIN_WIDTH}px; top: ${render.location.y + PIN_SCALE}px;`;
+  const element = pinTemplate.cloneNode(true);
+  element.style = `left: ${render.location.x}px; top: ${render.location.y + PIN_SCALE}px;`;
   element.querySelector('img').src = render.author.avatar;
   element.querySelector('img').alt = render.offer.title;
 
